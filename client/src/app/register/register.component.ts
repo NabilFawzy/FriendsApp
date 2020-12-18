ae0a544551fe872104f10ae33617a076e353ed6e
Nabil Fawzy <nabilfawzy507@gmail.com>
2020-12-18 09:43:54 +0200

    End of Routing 6

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,7 +11,7 @@ export class RegisterComponent implements OnInit {
  // @Input() usersFromHomeComponent:any;
   @Output() cancelRegister=new EventEmitter();
   model:any={};
-  constructor(public accountService:AccountService) { }
+  constructor(public accountService:AccountService,private toastr:ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +20,8 @@ export class RegisterComponent implements OnInit {
        console.log(Response);
        this.cancel();
      },error=>{
-       console.log(error)
+       console.log(error);
+       this.toastr.error(error.error);
      })
   }
  
