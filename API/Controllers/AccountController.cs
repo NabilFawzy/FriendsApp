31b1e1f6386980b45e716652bf20dbd3b368263f
Nabil Fawzy <nabilfawzy507@gmail.com>
2021-01-04 03:11:24 +0200

    Update Images 12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,31 +17,37 @@ namespace API.Controllers
     {
         private readonly DataContext _Context;
         private readonly ITokenService _tokenService;
-        public AccountController(DataContext context, ITokenService tokenService)
+        private readonly IMapper _mapper;
+        public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
         {
+            this._mapper = mapper;
             this._tokenService = tokenService;
-           this. _Context = context;
+            this._Context = context;
         }
 
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await checkExist(registerDto.Username)) return BadRequest("Username is exist");
+             
+            var user=_mapper.Map<AppUser>(registerDto);
+
             using var hmac = new HMACSHA512();
 
-            var user = new AppUser
-            {
-                UserName = registerDto.Username.ToLower(),
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.password)),
-                PasswordSalt = hmac.Key
-            };
+            
+            user.UserName = registerDto.Username.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.password));
+            user.PasswordSalt = hmac.Key;
+            
 
             _Context.Users.Add(user);
             await _Context.SaveChangesAsync();
 
-            return new UserDto{
-                Username=user.UserName,
-                Token=_tokenService.CreateToken(user)
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user),
+                KnownAs=user.KnownAs
             };
 
         }
@@ -54,7 +61,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _Context.Users
-            .Include(p=>p.Photos)
+            .Include(p => p.Photos)
             .SingleOrDefaultAsync(
                 x => x.UserName == loginDto.Username
                 );
@@ -70,10 +77,12 @@ namespace API.Controllers
                     return Unauthorized("Invalid Password");
             }
 
-            return new UserDto(){
-                Username=user.UserName,
-                Token=_tokenService.CreateToken(user),
-                PhotoUrl=user.Photos.FirstOrDefault(x=>x.isMain)?.Url
+            return new UserDto()
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.isMain)?.Url,
+                KnownAs=user.KnownAs
             };
         }
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,28 +1,78 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+ 
 })
 export class RegisterComponent implements OnInit {
  // @Input() usersFromHomeComponent:any;
+ registerForm:FormGroup; 
+ bsConfig: Partial<BsDatepickerConfig>;//date picker
+ maxDate:Date;
+ validationErrors:string[]=[];
   @Output() cancelRegister=new EventEmitter();
-  model:any={};
-  constructor(public accountService:AccountService,private toastr:ToastrService) { }
-
+ 
+  constructor(public accountService:AccountService,private toastr:ToastrService
+    ,private fb:FormBuilder,private router:Router) { }
+   
   ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate=new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear()-18);
   }
+   
+  initializeForm(){
+    
+    //datepicker
+    this.bsConfig =  { 
+      containerClass: "theme-blue",
+      dateInputFormat:'DD MMMM YYYY'
+     };
+    this.registerForm=this.fb.group({
+      gender:['male'],
+      username:['',Validators.required],
+      knownAs:['',Validators.required],
+      dateOfBirth:['',Validators.required],
+      city:['',Validators.required],
+      country:['',Validators.required],
+      password:['',
+      [Validators.required,Validators.minLength(4),Validators.maxLength(8)]],
+      confirmPassword:['',
+      [Validators.required,this.matchValues('password')]]
+    })
+  }
+
+  
+  private matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+     
+      if (control.parent && control.parent.controls) {
+        return control?.value ===
+          (control?.parent?.controls as { [key: string]: AbstractControl })[ matchTo ].value ? null : { isMatching: true };
+      }
+
+      return null;
+    };
+  }
+
+ 
+
+
   register(){
-     this.accountService.register(this.model).subscribe(Response=>{
-       console.log(Response);
+     this.accountService.register(this.registerForm.value).subscribe(Response=>{
+       this.router.navigateByUrl('/members')
        this.cancel();
      },error=>{
-       console.log(error);
-       this.toastr.error(error.error);
-     })
+       this.validationErrors=error
+     }) 
   }
  
  
